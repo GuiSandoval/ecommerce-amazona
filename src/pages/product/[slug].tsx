89fffcd14e94cdbf3b22ac4d7ba1ab18c48e3736
GuiSandoval <guijpjs@gmail.com>
2022-04-30 23:46:56 -0300

    chore ✨: Create Theme Desingn

--- src/pages/product/[slug].tsx
+++ src/pages/product/[slug].tsx
@@ -51,7 +51,9 @@ const ProductScreen: NextPage = () => {
         <Grid item md={3} xs={12}>
           <List>
             <ListItem>
-              <Typography component="h1">{product.name}</Typography>
+              <Typography component="h1" variant="h1">
+                {product.name}
+              </Typography>
             </ListItem>
             <ListItem>
               <Typography>Category: {product.category}</Typography>
