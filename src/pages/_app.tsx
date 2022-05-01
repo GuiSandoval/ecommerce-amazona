0eaa575a169f0f14abbc5cf9d3a7e3e1a3c27990
GuiSandoval <guijpjs@gmail.com>
2022-04-30 22:19:33 -0300

    chore ✨: Fix MaterialUi render Styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
 import "../styles/global.css";
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-);
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
 
+  return <Component {...pageProps} />;
+};
 export default MyApp;
